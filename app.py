390535a567cf80e3d92ee519cf7a480fd8a508a8
kiganyamburu <mburukiganya@gmail.com>
2025-05-08 19:56:37 +0300

    done

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 
+
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
